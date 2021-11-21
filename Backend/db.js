07ac3e7fe15b1c4b41389ddb45f35670160562d3
Ram Shingane <ramshingane@local>
2021-11-21 19:10:29 +0530

    Adding data validator

--- Backend/db.js
+++ Backend/db.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
 
 
-const mongOURI = "mongodb://localhost:27017/?readPreference=primary&appname=MongoDB%20Compass&directConnection=true&ssl=false";
+const mongOURI = "mongodb://localhost:27017/mynotebook?readPreference=primary&appname=MongoDB%20Compass&directConnection=true&ssl=false";
 
 const connectToMongo = () =>{
     mongoose.connect(mongOURI, ()=>{
--- Backend/routes/auth.js
+++ Backend/routes/auth.js
@@ -1,14 +1,32 @@
 const express = require('express');
-const user = require('../models/User');
+const User = require('../models/User');
 const router = express.Router();
+const { body, validationResult } = require('express-validator');
 
 //Create a user using: POST "/api/auth/". Doesnt require auth
-router.get('/',(req,res)=>{
+router.get('/',[
+    body('name',"Enter a valid name").isLength({min: 3}),
+    body('email',"Enter a valid email").isEmail(),
+    body('password',"Password must be atleast 5 characters").isLength({min: 5}),
+],(req,res)=>{
 
-    console.log(req.body);
-    const User = user(req.body);
-    User.Save();
-    res.send(req.body);
+    // Finds the validation errors in this request and wraps them in an object with handy functions
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
+    }
+
+    User.create({
+        name: req.body.name,
+        password: req.body.password,
+        email: req.body.email
+
+    }).then(user => res.json(user))
+    .catch(err=> console.log(err),
+    res.json({error: 'Please unique value for email'}));
+    
+
+   
 });
 
 module.exports = router;
